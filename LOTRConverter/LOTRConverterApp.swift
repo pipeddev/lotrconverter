277aebfeee48d59f03011828d4016105c16235b1
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2025-06-27 23:21:44 -0400

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Felipe Carrasco on 27-06-25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LOTRConverterTests/LOTRConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterTests.swift
+//  LOTRConverterTests
+//
+//  Created by Felipe Carrasco on 27-06-25.
+//
+
+import Testing
+@testable import LOTRConverter
+
+struct LOTRConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
